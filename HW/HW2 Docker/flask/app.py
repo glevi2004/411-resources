25ca85e2aa89f53030abe4ca5af08acb8e278253
Levi Ramos <glevi@bu.edu>
2025-02-11 19:12:12 -0500

    finished hw1 flask section and added respective screenshots from the process

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -1,4 +1,10 @@
-from flask import Flask, make_response
+from mimetypes import init
+from types import MethodDescriptorType
+from urllib import response
+from flask import Flask, make_response, request, jsonify
+import os
+import time
+
 
 app = Flask(__name__)
 
@@ -12,8 +18,35 @@ def hello():
     )
     return response
 
+# New '/repeat' route to handle GET parameter "input
+@app.route('/repeat', methods=['GET'])
+def repeat():
+    user_input = request.args.get('input', 'No input provided')
+    reponse = {
+        "body": user_input,
+        "status": 200
+    }
+    return jsonify(reponse)
+
+# Expose one function at both '/health' and '/healthcheck'
+@app.route('/health')
+@app.route('/healthcheck')
+def health():
+    return jsonify({"body": "OK", "status": 200}) #shared function for both endpoints
+
+# New '/hang' endpoint that causes the server to freeze
+@app.route('/hang')
+def hang():
+    while True: # Infinite loop (intentionally crashing Flask)
+        time.sleep(1)
+
+
 if __name__ == '__main__':
     # By default flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.getenv("PORT", 5002)) # Default o 5002 if port not setyp since I am using mac
+    app.run(host='0.0.0.0', port=port, debug=True, threaded=False) # Run Flask in single-threaded mode
+    # flask normally handles mutiple requests in seperate threads
+    # this ensures when /hand is called entire server will freeze
+
